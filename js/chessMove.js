ad930691ef6ad7be43a55b712424f33cb974ab58
Swapnil Kisan Gadave <120236499+sgadave@users.noreply.github.com>
2022-12-20 21:59:31 +0530

    Update chessMove.js

--- js/chessMove.js
+++ js/chessMove.js
@@ -240,12 +240,12 @@ function endGame(){
     if(player=="white"){
        if(defeatedBlackPiecesId.includes("blkking")){
         console.log("Whitewins");
-        window.open('../chess.github.io/whiteSideWins.html',"_self");
+        window.open('chess.github.io/html/whiteSideWins.html',"_self");
        }
     }else{
         if(defeatedWhitePiecesId.includes("whtking")){
             console.log("Blackwins");
-            window.open('../chess.github.io/blackSideWins.html',"_self");
+            window.open('chess.github.io/html/blackSideWins.html',"_self");
         }
 
     }//if ends here
